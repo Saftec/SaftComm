49d30efd5883ea08f35524ac98184bc2f31916dc
Sergio <Sergio@SAF05>
2016-08-31 12:53:17 -0300

    Se agrega campo de baja en tabla empleados.

--- ZkManagemente/ZkManagemente/Datos/CatalogoEmpleados.cs
+++ ZkManagemente/ZkManagemente/Datos/CatalogoEmpleados.cs
@@ -19,7 +19,7 @@ namespace ZkManagement.Datos
             {
                 conn = con.Conectar();
                 //Ya lo traigo ordenado alfabeticamente desde la BD.
-                string consulta = "SELECT e.Legajo, e.IdEmpleado, e.Nombre, e.Tarjeta, e.DNI, e.Pin, e.Privilegio,COUNT(h.IdEmpleado) as 'Cant' FROM Empleados e LEFT JOIN Huellas h ON e.IdEmpleado = h.IdEmpleado GROUP BY e.IdEmpleado, e.Nombre, e.Pin, e.Tarjeta, e.Legajo, e.DNI, e.privilegio ORDER BY e.Nombre ASC";                 
+                string consulta = "SELECT e.Legajo, e.IdEmpleado, e.Nombre, e.Tarjeta, e.DNI, e.Pin, e.Privilegio,COUNT(h.IdEmpleado) as 'Cant' FROM Empleados e LEFT JOIN Huellas h ON e.IdEmpleado = h.IdEmpleado WHERE e.Baja=0 GROUP BY e.IdEmpleado, e.Nombre, e.Pin, e.Tarjeta, e.Legajo, e.DNI, e.privilegio ORDER BY e.Nombre ASC";                 
                 SqlCommand cmd = new SqlCommand(consulta,conn);
                 SqlDataAdapter da = new SqlDataAdapter(cmd);
                 da.Fill(empleados);
