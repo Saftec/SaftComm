2ca0fc6c166f3a8688b84d001abec8086e5d2043
Sergio <Sergio@SAF05>
2016-09-29 13:32:44 -0300

    Creación de proyecto para instalar base de datos.

---
+++ ZkManagemente/CreateDataBase/Interfaz/Principal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateDataBase
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ZkManagemente/CreateDataBase/Logica/LogicDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace CreateDataBase.Logica
+{
+    class LogicDatabase
+    {
+        public void CrearBase(string usuario, string pass, string servidor, string bd, string time)
+        {
+            string sqlConnectionString = "Data Source="+ servidor + ";Initial Catalog="+ bd +"; User ID="+usuario+";Password="+pass+"; Connection Timeout="+time;
+            string script = File.ReadAllText(@"\CreateDB.sql");
+
+            SqlConnection conn = new SqlConnection(sqlConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(script, conn);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
