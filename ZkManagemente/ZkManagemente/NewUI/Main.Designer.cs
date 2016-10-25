6518f0f7f73773f65a228e9f4eea62594b992909
Sergio <Sergio@SAF05>
2016-10-25 12:23:40 -0300

    Pruebas con MetroFramework

--- ZkManagemente/ZkManagemente/NewUI/Main.Designer.cs
+++ ZkManagemente/ZkManagemente/NewUI/Main.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.lblOff = new MetroFramework.Controls.MetroLabel();
+            this.btnDispositivos = new MetroFramework.Controls.MetroTile();
             this.SuspendLayout();
             // 
             // lblOff
@@ -40,14 +41,26 @@
             this.lblOff.TabIndex = 0;
             this.lblOff.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
+            // btnDispositivos
+            // 
+            this.btnDispositivos.ActiveControl = null;
+            this.btnDispositivos.BackColor = System.Drawing.Color.White;
+            this.btnDispositivos.Location = new System.Drawing.Point(184, 95);
+            this.btnDispositivos.Name = "btnDispositivos";
+            this.btnDispositivos.Size = new System.Drawing.Size(75, 23);
+            this.btnDispositivos.TabIndex = 1;
+            this.btnDispositivos.Text = "Dispositivos";
+            this.btnDispositivos.UseSelectable = true;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(869, 638);
+            this.Controls.Add(this.btnDispositivos);
             this.Controls.Add(this.lblOff);
             this.Name = "Main";
-            this.Text = "Main";
+            this.Text = "SaftComm";
             this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -57,5 +70,6 @@
         #endregion
 
         private MetroFramework.Controls.MetroLabel lblOff;
+        private MetroFramework.Controls.MetroTile btnDispositivos;
     }
 }
