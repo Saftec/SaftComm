d1a7697b027bb97e5d3c130e5f334732135a819d
Lazaro Ansaldi <cuervo.piedritas@gmail.com>
2017-12-16 12:39:30 -0300

    Add DbContext

---
+++ ZkManagemente/Database/DBContext/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Database.DBContext
+{
+    class DatabaseContext : DbContext
+    {
+        private const string DB_CONTEXT = "DatabaseContext";
+
+        public DatabaseContext() : base(DB_CONTEXT)
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
