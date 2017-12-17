db38a49f9a0713bb151f99f93f612303eddef13c
Lazaro Ansaldi <cuervo.piedritas@gmail.com>
2017-12-16 22:08:56 -0300

    Add base repository interface

---
+++ ZkManagemente/Database/BaseRepository/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using Entidades;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Database.BaseRepository
+{
+    interface IBaseRepository<T> where T : BaseEntity
+    {
+        Task InsertOne(T entity);
+
+        Task<IEnumerable<T>> GetAll();
+
+        Task<T> GetOneById(int id);
+
+        Task DeleteOneById(int id);
+
+        Task UpdateOne(T entity);
+    }
+}
